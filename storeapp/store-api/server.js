aac35d0cc29266a1bfd4b3dcb32256ebef0187f6
Abdilmajid Ibrahim <abdilwebdev@gmail.com>
2023-08-10 10:22:16 -0400

    fixed build issue for store-app

--- storeapp/store-api/server.js
+++ storeapp/store-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config()
 
 
@@ -9,18 +10,25 @@ const app = express()
 
 
 
-// localhost../..html is served from public folder
-app.use(express.static('./public'));
+
 // Extended true ->is a nested  
 // Extended False ->not nested object
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+// localhost../..html is served from public folder
+// app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'store-web', 'build', 'index.html'));
+});
 
 //importing routes from different file
 const router = require('./routes')
 app.use(router)
+
 
 
 app.listen(process.env.PORT || 4001, ()=> {
---
+++ storeapp/store-web/build/precache-manifest.37ff185fa100477701511893be01c3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1521e9a5462073be1f27",
+    "url": "./static/css/main.c12475d4.chunk.css"
+  },
+  {
+    "revision": "1521e9a5462073be1f27",
+    "url": "./static/js/main.1521e9a5.chunk.js"
+  },
+  {
+    "revision": "42ac4df1063a76579e96",
+    "url": "./static/js/1.42ac4df1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2de3171b64d23a601f148687c347c01f",
+    "url": "./index.html"
+  }
+];
